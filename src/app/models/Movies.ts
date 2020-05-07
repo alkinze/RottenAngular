e3b7eee50810c9f3581a8d88612f01277f64a357
greeneterrance92 <58027070+greeneterrance92@users.noreply.github.com>
2020-05-07 03:29:38 -0500

    recommendation modal and recommendations functionality

--- src/app/models/Movies.ts
+++ src/app/models/Movies.ts
@@ -1,26 +1,26 @@
 export class Movies {
 
-    movieid:number;
-    moviename:string;
+    movieid : number;
+    moviename : string;
 
-    constructor(movieid:number, moviename:string){
+    constructor(movieid : number, moviename : string) {
         this.movieid = movieid;
         this.moviename = moviename;
     }
 
-    public getMovieid():number{
+    public getMovieid(): number {
         return this.movieid;
     }
 
-    public setMovieid(movieid:number){
+    public setMovieid(movieid : number) {
         this.movieid = movieid;
     }
 
-    public getMoviename():string{
+    public getMoviename(): string {
         return this.moviename;
     }
 
-    public setMoviename(moviename:string){
+    public setMoviename(moviename : string) {
         this.moviename = moviename;
     }
 }
